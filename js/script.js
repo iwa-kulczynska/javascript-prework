459490a2839e46992b1bcddd33db48714fe22f3d
Iwona Kulczynska <iwa.kulczynska@gmail.com>
2020-09-24 23:58:37 +0200

    add answer the gamer

--- js/script.js
+++ js/script.js
@@ -8,7 +8,37 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
+
+
 var computerMove, playerMove;
-playerMove = 'papier';
-computerMove = 'kamień';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+playerMove = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + playerMove);
+if (playerMove  == '1') {
+  computerMove = 'kamień';
+} else if (playerMove  == '2'){
+    computerMove = 'papier';
+}
+else if (playerMove  == '3'){
+    computerMove = 'nożyce';
+}
+else {
+  computerMove = 'remis';
+}
+printMessage('Mój ruch: ' + computerMove);
+
+
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2'){
+    playerMove = 'nożyce';
+}  else if (playerInput == '3'){
+    playerMove = 'papier';
+}
+else {
+  playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
