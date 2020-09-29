64cdac887200282842a5d54da037eb8f0e7a0cd5
Iwona Kulczynska <iwa.kulczynska@gmail.com>
2020-09-29 13:13:18 +0200

    switch next var to let

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function printMessage(msg){
-	var div = document.createElement('div');
+	let div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
 }
@@ -7,8 +7,6 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
-
-
 
 
 
